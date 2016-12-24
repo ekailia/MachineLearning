0f42c6b96cbf4be73f1fcc3788582475695c654b
Hari Seldon <hari@Trantor>
2016-12-24 17:38:59 +0800

    Finalize kNN

--- kNN.py
+++ kNN.py
@@ -31,3 +31,36 @@ def autoNorm(dataSet):
 	normDataSet = dataSet - tile(minVals, (m, 1))
 	normDataSet = normDataSet/tile(ranges, (m, 1))
 	return normDataSet, ranges, minVals
+
+def file2matrix(filename):
+	fr = open(filename)
+	arrayOLines = fr.readlines()
+	numberOfLines = len(arrayOLines)
+	returnMat = zeros((numberOfLines, 3))
+	classLabelVector = []
+	index = 0
+	for line in arrayOLines:
+		line = line.strip()
+		listFromLine = line.split('\t')
+		returnMat[index,:] = listFromLine[0:3]
+		classLabelVector.append(int(listFromLine[-1]))
+		index += 1
+	return returnMat, classLabelVector
+
+def datingClassTest():
+	hoRatio = 0.10
+	datingDataMat, datingLabels = file2matrix("datingTestSet2.txt")
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	numTestVecs = int(m*hoRatio)
+	errorCount = 0.0
+	for i in range(numTestVecs):
+		classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m, :], \
+			datingLabels[numTestVecs:m],3)
+		print("The classifier came back with :%d, the real answer is :%d"\
+			% (classifierResult, datingLabels[i]))
+		if (classifierResult != datingLabels[i]): 
+			errorCount += 1.0
+	print("The total error rate is : %f" % (errorCount/float(numTestVecs)))
+	return
+
